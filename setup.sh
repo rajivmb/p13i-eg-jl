7e170d9319850203aa0a01e9bfadeacaedc963fb
rajiv <17132180+rajivmb@users.noreply.github.com>
2020-08-24 23:02:47 +1000

    Added duration calculations

--- setup.sh
+++ setup.sh
@@ -3,26 +3,19 @@
 #<Ref/> https://stackoverflow.com/a/8352939
 
 layerStart=$(date +%s)
-
 printf "\n\nSetting up Lambda Layer\n***********************\n\n"
-
 /bin/bash p13i-mit-eg-jpll/setup.sh
-
 functionStart=$(date +%s)
 layerDuration=$((functionStart - layerStart))
 printf "\nLambda Layer setup completed in              [ %02dm %02ds ]\n\n" $((layerDuration / 60)) $((layerDuration % 60))
+
 printf "\n\nSetting up Lambda Function\n**************************\n\n"
-
 /bin/bash p13i-mit-eg-jglf/setup.sh
-
-
-
 invokeStart=$(date +%s)
 functionDuration=$((invokeStart - functionStart))
 printf "\nLambda Function setup completed in           [ %02dm %02ds ]\n\n" $((functionDuration / 60)) $((functionDuration % 60))
 
 printf "\n\nInvoking Lambda Function\n************************\n\n"
-
 /bin/bash p13i-mit-eg-jglf/invoke.sh
 
 totalDuration=$(($(date +%s) - layerStart))
--- teardown.sh
+++ teardown.sh
@@ -2,10 +2,21 @@
 
 #<Ref/> https://stackoverflow.com/a/8352939
 
-printf "\n\nTearing down Lambda Function"
-
+functionStart=$(date +%s)
+printf "\n\nTearing down Lambda Function\n****************************\n\n"
 /bin/bash p13i-mit-eg-jglf/teardown.sh
+layerStart=$(date +%s)
+functionDuration=$((layerStart - functionStart))
+printf "\nLambda Function teardown completed in           [ %02dm %02ds ]\n\n" $((functionDuration / 60)) $((functionDuration % 60))
 
-printf "\n\nTearing down Lambda Layer"
-
+printf "\n\nTearing down Lambda Layer\n*************************\n\n"
 /bin/bash p13i-mit-eg-jpll/teardown.sh
+layerDuration=$(($(date +%s) - layerStart))
+printf "\nLambda Layer teardown completed in              [ %02dm %02ds ]\n\n" $((layerDuration / 60)) $((layerDuration % 60))
+
+
+totalDuration=$(($(date +%s) - functionStart))
+printf "\n\n"
+printf "Lambda Function teardown completed in           [ %02dm %02ds ]\n" $((functionDuration / 60)) $((functionDuration % 60))
+printf "Lambda Layer teardown completed in              [ %02dm %02ds ]\n" $((layerDuration / 60)) $((layerDuration % 60))
+printf "Teardown completed in                           [ %02dm %02ds ]\n\n" $((totalDuration / 60)) $((totalDuration % 60))
