701b82c9b4ddc928c7c7184196ffe76ce8495d6d
rajiv <17132180+rajivmb@users.noreply.github.com>
2020-08-24 22:49:46 +1000

    Added duration calculations

--- setup.sh
+++ setup.sh
@@ -2,14 +2,31 @@
 
 #<Ref/> https://stackoverflow.com/a/8352939
 
-printf "\n\nSetting up Lambda Layer"
+layerStart=$(date +%s)
+
+printf "\n\nSetting up Lambda Layer\n***********************\n\n"
 
 /bin/bash p13i-mit-eg-jpll/setup.sh
 
-printf "\n\nSetting up Lambda Function"
+functionStart=$(date +%s)
+layerDuration=$((functionStart - layerStart))
+printf "\nLambda Layer setup completed in              [ %02dm %02ds ]\n\n" $((layerDuration / 60)) $((layerDuration % 60))
+printf "\n\nSetting up Lambda Function\n**************************\n\n"
 
 /bin/bash p13i-mit-eg-jglf/setup.sh
 
-printf "\n\nInvoking Lambda Function"
+
+
+invokeStart=$(date +%s)
+functionDuration=$((invokeStart - functionStart))
+printf "\nLambda Function setup completed in           [ %02dm %02ds ]\n\n" $((functionDuration / 60)) $((functionDuration % 60))
+
+printf "\n\nInvoking Lambda Function\n************************\n\n"
 
 /bin/bash p13i-mit-eg-jglf/invoke.sh
+
+totalDuration=$(($(date +%s) - layerStart))
+printf "\n\n"
+printf "Lambda Layer setup completed in              [ %02dm %02ds ]\n" $((layerDuration / 60)) $((layerDuration % 60))
+printf "Lambda Function setup completed in           [ %02dm %02ds ]\n" $((functionDuration / 60)) $((functionDuration % 60))
+printf "Setup completed in                           [ %02dm %02ds ]\n\n" $((totalDuration / 60)) $((totalDuration % 60))
